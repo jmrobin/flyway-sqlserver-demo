f6794477320d24f75e15bcdb5758c10b8ca30590
Jean-Marc Robin <jmr33bx@yahoo.fr>
2017-01-16 07:58:23 +0100

    Add backup & restore script for before and after

---
+++ restore_dev_after.ps1
@@ -0,0 +1,12 @@
+Push-Location
+Import-Module SQLPS -DisableNameChecking
+Pop-Location
+$serverPath = "SQLSERVER:\SQL\localhost\SQLEXPRESS"
+$databaseName = "Payroll"
+$restoreFrom = Join-Path (Get-Location) "$databaseName-after.bak"
+$databasePath = Join-Path $serverPath "Databases\$databaseName"
+if(Test-Path $databasePath)
+{
+    Invoke-Sqlcmd "USE [master];ALTER DATABASE [$databaseName] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;DROP DATABASE [$databaseName]"
+}
+Restore-SqlDatabase -Path $serverPath -Database $databaseName -BackupFile $restoreFrom
